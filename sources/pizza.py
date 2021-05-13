adaf34fbeb936f827d82fca07f2ece09fd77f227
Pantomik <addr@domain.ext>
2021-05-13 20:31:25 +0200

    Pizza orderring and Tests

--- sources/pizza.py
+++ sources/pizza.py
@@ -1,12 +1,32 @@
 
 RECIPES = {
+    'marinara': {
+        'ingredients': {'garlic': 2, 'basil': 3},
+        'price': 9,
+    },
+    'romana': {
+        'ingredients': {'ham': 4, 'olive': 5, 'onion': 2, 'basil': 2},
+        'price': 12,
+    },
+    'original': {
+        'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
+        'price': 10,
+        'sauce': 'cream'
+    },
+    'ultima': {
+        'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
+        'price': 12,
+        'sauce': 'cream'
+    },
+    'mediterranea': {
+        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
+        'price': 11,
+    },
     'margherita': {
         'ingredients': {'mozzarella': 2, 'oregano': 4},
         'price': 10,
         'sauce': 'tomato'
     },
-
-
     'marinara': {
         'ingredients': {'garlic': 2, 'basil': 3},
         'price': 9,
@@ -43,11 +63,5 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
-    },
-    'my_pizza': {
-        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
-        'price': 11,
     }
-
-
 }
